9f6143859b74161bbe64f57eaa34cc5ac9ba1a13
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-12 23:07:06 +0200

    Progress fleshing out struct basics

--- contracts/VestCore.sol
+++ contracts/VestCore.sol
@@ -12,5 +12,32 @@ import '@openzeppelin/contracts/access/Ownable.sol';
     @title VestCore
  */
 contract VestCore is Ownable {
+	uint256 public constant SCALE = 1e18;
+	uint25 public fee = 1e15; // 0.1% fee
+
+	struct VestingToken {
+		address token;
+		address[] recipients;
+		uint256[] amounts;
+		uint256[] startTimes;
+		uint256[] endTimes;
+	}
+
+	mapping(uint256 => VestingToken) private vestingTokens;
+	// isAdminOfVestingToken[account][vestingTokenId] = true/false
+	mapping(address => mapping(uint256 => bool)) private isAdminOfVestingToken;
+
 	constructor() {}
+
+	// TODO better name?
+	function createVestingAgreement(
+		address _token,
+		address[] _recipients,
+		uint256[] _amounts,
+		uint256[] _startTimes,
+		uint256[] _endTimes
+	) returns (bool success) {
+		// TODO
+		return true;
+	}
 }
