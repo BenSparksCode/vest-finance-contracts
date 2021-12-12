70da41a1495c7be5d474eafb4ef808a927b04be2
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-12 22:46:04 +0200

    Starting VestCore v0.1 contract

---
+++ contracts/VestCore.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+// ------------------------------------------ //
+//                VestCore v0.1               //
+// ------------------------------------------ //
+
+/**
+    @title VestCore
+ */
+contract VestCore is Ownable {
+	constructor() {}
+}
