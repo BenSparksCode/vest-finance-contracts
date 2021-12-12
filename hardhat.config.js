1ee9757f53e9f1c0d48e0fbc889c73241479d987
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-12 22:45:50 +0200

    Setting up Hardhat env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
-require('hardhat-contract-sizer');
+require("hardhat-contract-sizer");
 require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-etherscan");
 
-require('dotenv').config();
+require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
@@ -14,7 +14,7 @@ task("accounts", "Prints the list of accounts", async () => {
 });
 
 module.exports = {
-  solidity: "0.8.6",
+  solidity: "0.8.10",
   loggingEnabled: true,
   contractSizer: {
     alphaSort: true,
@@ -22,7 +22,7 @@ module.exports = {
     disambiguatePaths: false,
   },
   gasReporter: {
-    endabled: true
+    endabled: true,
   },
   defaultNetwork: "localhost",
   networks: {
@@ -31,26 +31,30 @@ module.exports = {
     },
     hardhat: {
       forking: {
-        url: "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_API,
-        blockNumber: 17748900
-      }
+        url:
+          "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_API,
+        blockNumber: 19872200,
+      },
+      accounts: {
+        accountsBalance: "1000000000000000000000000", //1 million ETH to signers
+      },
+      gasPrice: 0,
     },
     mumbai: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_API,
       accounts: [`${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
       chainId: 80001,
-      gas: 5000000,     // 5 million
-      gasLimit: 5000000,     // 5 million
-      gasPrice: 5000000000   // 5 gwei
+      gas: 5000000, // 5 million
+      gasLimit: 5000000, // 5 million
+      gasPrice: 5000000000, // 5 gwei
     },
     polygon: {
       url: "https://matic-mainnet.chainstacklabs.com",
       accounts: [`${process.env.POLYGON_DEPLOYER_PRIV_KEY}`],
       chainId: 137,
-    }
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
-
--- test/TestConstants.js
+++ test/TestConstants.js
@@ -2,59 +2,46 @@ const { BigNumber } = require("@ethersproject/bignumber");
 const { ethers } = require("hardhat");
 
 const CONSTANTS = {
-    MUMBAI: {
-        DAI: "0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F",
-        aDAI: "0x639cB7b21ee2161DF9c882483C9D55c90c20Ca3e",
-        LINK: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-        ChainlinkVRFCoordinator: "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255",
-        ChainlinkKeyHash: "0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4",
-        AaveLendingPool: "0x9198F13B08E299d85E096929fA9781A1E3d5d827",
-        ZoraMedia: "0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7",
-        ZoraMarket: "0xE5BFAB544ecA83849c53464F85B7164375Bdaac1",
-    },
-    POLYGON: {
-        DAI: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
-        aDAI: "0x27F8D03b3a2196956ED754baDc28D73be8830A6e",
-        LINK: "0xb0897686c545045afc77cf20ec7a532e3120e0f1",
-        ChainlinkVRFCoordinator: "0x3d2341ADb2D31f1c5530cDC622016af293177AE0",
-        ChainlinkKeyHash: "0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da",
-        AaveLendingPool: "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf",
-        ZoraMedia: "0x6953190AAfD8f8995e8f47e8F014d0dB83E92300",
-        ZoraMarket: "0xE20bd7dC76e09AEBC2A9A732AB6AEE616c5a17Eb",
-    },
+  MUMBAI: {
+    DAI: "0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F",
+    aDAI: "0x639cB7b21ee2161DF9c882483C9D55c90c20Ca3e",
+  },
+  POLYGON: {
+    DAI: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+    aDAI: "0x27F8D03b3a2196956ED754baDc28D73be8830A6e",
+  },
+  SHIP: {
+    decimals: 18,
+    total: 100000000, //  100 million
+    hackathonAirdrop: 2000000, //  2 million
+    mainnetAirdrop: 8000000, //  8 million
+    strategicPartners: 16000000, //  16 million
+    stakingRewards: 20000000, //  20 million
+    teamVesting: 24000000, //  24 million
+    daoTreasury: 30000000, //  30 million
+  },
+  DEPLOY: {
     SHIP: {
-        decimals: 18,
-        total: 100000000,               //  100 million
-        hackathonAirdrop: 2000000,      //  2 million
-        mainnetAirdrop: 8000000,        //  8 million
-        strategicPartners: 16000000,    //  16 million
-        stakingRewards: 20000000,       //  20 million
-        teamVesting: 24000000,          //  24 million
-        daoTreasury: 30000000           //  30 million
+      name: "SHIP",
+      symbol: "SHIP",
+      totalSupply: ethers.utils.parseUnits("100000000", "ether"), //100 million with 18 decimals
     },
-    DEPLOY: {
-        SHIP:{
-            name: "SHIP",
-            symbol: "SHIP",
-            totalSupply: ethers.utils.parseUnits("100000000", "ether") //100 million with 18 decimals
-        },
-        FERRY: {
-            annualFee: ethers.utils.parseUnits("24", "ether"), //$24 per year to start
-            maxMintedNFTs: 50000,
-            maxMembershipPeriod: 2*365*86400, // 2 years = 2 * 365 days * 86400 seconds per day
-            nftThreshold: ethers.utils.parseUnits("1", "ether"), //$1 Will cost less than $0.01 in LINK for random num
-        },
-        NFT_MINTER: {
-            vrfFee: 100000000000000,
-        },
-        TOKENS: {
-            daiApproveAmount: ethers.utils.parseUnits("30", "ether"),
-            linkToMinterAmount: 1000000000000000 // 10 * 0.0001 VRF fee
-        }
-    }
-}
-
+    FERRY: {
+      annualFee: ethers.utils.parseUnits("24", "ether"), //$24 per year to start
+      maxMintedNFTs: 50000,
+      maxMembershipPeriod: 2 * 365 * 86400, // 2 years = 2 * 365 days * 86400 seconds per day
+      nftThreshold: ethers.utils.parseUnits("1", "ether"), //$1 Will cost less than $0.01 in LINK for random num
+    },
+    NFT_MINTER: {
+      vrfFee: 100000000000000,
+    },
+    TOKENS: {
+      daiApproveAmount: ethers.utils.parseUnits("30", "ether"),
+      linkToMinterAmount: 1000000000000000, // 10 * 0.0001 VRF fee
+    },
+  },
+};
 
 module.exports = {
-    constants: CONSTANTS
-}
+  constants: CONSTANTS,
+};
